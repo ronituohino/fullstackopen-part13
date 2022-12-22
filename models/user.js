2f7ad8ff1b7e7d27049a1ccb8abcf6deb4c0be28
Roni Tuohino <tuohinoroni@gmail.com>
2022-12-22 15:36:39 +0200

    add: task 13.9

--- models/user.js
+++ models/user.js
@@ -8,7 +8,16 @@ User.init(
   {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.TEXT, allowNull: false },
-    username: { type: DataTypes.TEXT, unique: true, allowNull: false },
+    username: {
+      type: DataTypes.TEXT,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: {
+          msg: "Validation isEmail on username failed",
+        },
+      },
+    },
   },
   { sequelize, underscored: true, modelName: "user" }
 );
