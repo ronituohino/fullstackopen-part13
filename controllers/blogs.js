2eda691d9d1334b0989a0009f02ff125abf78dda
Roni Tuohino <tuohinoroni@gmail.com>
2022-12-22 16:08:46 +0200

    add: tasks up to 13.11.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,29 @@
 const router = require("express").Router();
 
-const { Blog } = require("../models");
+const { User, Blog } = require("../models");
+const { tokenExtractor } = require("../util/token");
 
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll();
   return res.json(blogs);
 });
 
-router.post("/", async (req, res) => {
-  const blog = await Blog.create(req.body);
+router.post("/", tokenExtractor, async (req, res) => {
+  console.log(req.decodedToken);
+  const user = await User.findByPk(req.decodedToken.id);
+  const blog = await Blog.create({ ...req.body, userId: user.id });
   return res.json(blog);
 });
 
-router.delete("/:id", async (req, res) => {
-  const blog = await Blog.destroy({ where: { id: req.params.id } });
-  return res.json(blog);
+router.delete("/:id", tokenExtractor, async (req, res) => {
+  await Blog.findByPk(req.params.id).then(blog => {
+    if (blog.userId === req.decodedToken.id) {
+      blog.destroy();
+      return res.json(blog);
+    } else {
+      throw new Error("User did not create this blog");
+    }
+  });
 });
 
 router.put("/:id", async (req, res) => {
