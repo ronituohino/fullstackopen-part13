3903f72d9e64312fde8c0da28b6cebc9c3f5644a
Roni Tuohino <tuohinoroni@gmail.com>
2022-12-22 16:48:38 +0200

    add: task 13.16.

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
 
+const { Op } = require("sequelize");
+
+const { Blog } = require("../models");
+const { sequelize } = require("../util/db");
+
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll({
+    group: "author",
+    attributes: [
+      "author",
+      [sequelize.fn("COUNT", sequelize.col("blog")), "blog_amount"],
+      [sequelize.fn("SUM", sequelize.col("likes")), "sum_likes"],
+    ],
+    where: {
+      author: {
+        [Op.ne]: null,
+      },
+    },
+  });
+  return res.json(blogs);
+});
+
 module.exports = router;
